162067d806c6b7ad7fc99bfbda21bac4485d6d6d
Daniel <dhonaruto@gmail.com>
2022-08-18 17:37:18 -0400

    create the exhibition page to be responsiive

--- showcase/script.js
+++ showcase/script.js
@@ -1,1 +1,22 @@
-    
+const showcase = document.getElementById('showcase');
+
+fetch('./projects.json')
+    .then(results => results.json())
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            const project = document.createElement('div')
+            project.classList.add('sub-container')
+
+            project.innerHTML = 
+            `
+            <img src="${data[i].image_src}" alt="${data[i].image_alt}">
+
+            <a class="title" href="${data[i].demo}">${data[i].title}</a>
+            <button class="btn">
+                <a class="repo" href="https://github.com/Daniel-Ho986/50-mini-projects/tree/main${data[i].repo}">Repository</a>
+            </button>
+            `
+            showcase.appendChild(project)
+        }
+        
+    });
