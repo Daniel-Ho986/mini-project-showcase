b30a14439d224b4c748ff83fdd56ee089d77908b
Daniel <dhonaruto@gmail.com>
2022-08-19 15:04:36 -0400

    Rename project 47

--- showcase/script.js
+++ showcase/script.js
@@ -9,11 +9,11 @@ fetch('./projects.json')
 
             project.innerHTML = 
             `
-            <img src="${data[i].image_src}" alt="${data[i].image_alt}">
+            <img src="/images/${data[i].image_src}" alt="${data[i].image_alt}">
 
-            <a class="title" href="${data[i].demo}">${data[i].title}</a>
+            <a class="title" href="/${data[i].demo}">${data[i].title}</a>
             <button class="btn">
-                <a class="repo" href="https://github.com/Daniel-Ho986/50-mini-projects/tree/main${data[i].repo}">Repository</a>
+                <a class="repo" href="https://github.com/Daniel-Ho986/50-mini-projects/tree/main/${data[i].repo}">Repository</a>
             </button>
             `
             showcase.appendChild(project)
