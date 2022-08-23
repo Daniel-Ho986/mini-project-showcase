18aa1a66bf308834531f4c526deca26a08bc484c
Daniel <dhonaruto@gmail.com>
2022-08-23 14:19:52 -0400

    Update showcase page

--- showcase/script.js
+++ showcase/script.js
@@ -10,7 +10,6 @@ fetch('./projects.json')
             project.innerHTML = 
             `
             <img src="/images/${data[i].image_src}" alt="${data[i].image_alt}">
-
             <a class="title" href="/${data[i].demo}">${data[i].title}</a>
             <button class="btn">
                 <a class="repo" href="https://github.com/Daniel-Ho986/50-mini-projects/tree/main/${data[i].repo}">Repository</a>
