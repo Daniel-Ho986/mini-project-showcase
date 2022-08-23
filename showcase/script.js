ef39d9511f526b8715e3fd694c82864da3ea9ada
Daniel <dhonaruto@gmail.com>
2022-08-23 15:21:04 -0400

    Stored and update projects within showcase

--- showcase/script.js
+++ showcase/script.js
@@ -1,6 +1,6 @@
 const showcase = document.getElementById('showcase');
 
-fetch('./projects.json')
+fetch('./projects/projects.json')
     .then(results => results.json())
     .then(data => {
         for (let i = 0; i < data.length; i++) {
@@ -9,10 +9,10 @@ fetch('./projects.json')
 
             project.innerHTML = 
             `
-            <img src="/images/${data[i].image_src}" alt="${data[i].image_alt}">
-            <a class="title" href="/${data[i].demo}">${data[i].title}</a>
+            <img src="./images/${data[i].image_src}" alt="${data[i].image_alt}">
+            <a class="title" href="projects/${data[i].demo}">${data[i].title}</a>
             <button class="btn">
-                <a class="repo" href="https://github.com/Daniel-Ho986/50-mini-projects/tree/main/${data[i].repo}">Repository</a>
+                <a class="repo" href="https://github.com/Daniel-Ho986/50-mini-projects/tree/main/showcase/projects/${data[i].repo}">Repository</a>
             </button>
             `
             showcase.appendChild(project)
