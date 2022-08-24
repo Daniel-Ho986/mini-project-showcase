d0817bd85fd61e9f0a6eb861ab5691081222a01b
Daniel <dhonaruto@gmail.com>
2022-08-23 23:50:03 -0400

    update image file path

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const showcase = document.getElementById('showcase');
 
-fetch('./projects/projects.json')
+fetch('/projects/projects.json')
     .then(results => results.json())
     .then(data => {
         for (let i = 0; i < data.length; i++) {
@@ -9,7 +9,7 @@ fetch('./projects/projects.json')
 
             project.innerHTML = 
             `
-            <img src="/images/${data[i].image_src}" alt="${data[i].image_alt}">
+            <img src="./images/${data[i].image_src}" alt="${data[i].image_alt}">
             <a class="title" href="projects/${data[i].demo}">${data[i].title}</a>
             <button class="btn">
                 <a class="repo" href="https://github.com/Daniel-Ho986/50-mini-projects/tree/main/projects/${data[i].repo}">Repository</a>
