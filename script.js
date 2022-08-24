1019b7c9fffbd6e0c89231780a736550548c5f62
Daniel <dhonaruto@gmail.com>
2022-08-24 13:49:36 -0400

    Check github page

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const showcase = document.getElementById('showcase');
 
+const images = 
+[
+    {
+        "image_src": "1-expanding-cards.png",
+        "image_alt": "expanding-cards",
+    },
+    {
+        "image_src": "2-progress-steps.png",
+        "image_alt": "progress-steps",
+    },
+    {
+        "image_src": "3-rotating-navigation.png",
+        "image_alt": "rotating-navigation",
+    }
+]
+
 fetch('projects/projects.json')
     .then(results => results.json())
     .then(data => {
@@ -9,7 +25,7 @@ fetch('projects/projects.json')
 
             project.innerHTML = 
             `
-            <img src="/projects/images/${data[i].image_src}" alt="${data[i].image_alt}">
+            <img src="/projects/images/${images[i].image_src}" alt="${images[i].image_alt}"></img>
             <a class="title" href="projects/${data[i].demo}">${data[i].title}</a>
             <button class="btn">
                 <a class="repo" href="https://github.com/Daniel-Ho986/50-mini-projects/tree/main/projects/${data[i].repo}">Repository</a>
