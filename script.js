694981d1ff4b270539739e9016e99c3551df550c
Daniel <dhonaruto@gmail.com>
2022-08-24 00:17:01 -0400

    move image folder

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ fetch('/projects/projects.json')
 
             project.innerHTML = 
             `
-            <img src="./images/${data[i].image_src}" alt="${data[i].image_alt}">
+            <img src="/projects/images/${data[i].image_src}" alt="${data[i].image_alt}">
             <a class="title" href="projects/${data[i].demo}">${data[i].title}</a>
             <button class="btn">
                 <a class="repo" href="https://github.com/Daniel-Ho986/50-mini-projects/tree/main/projects/${data[i].repo}">Repository</a>
