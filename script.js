bbdc26a3aae9b586ef22ad2b82efe1dc13eb1ad1
Daniel <dhonaruto@gmail.com>
2024-02-12 23:15:30 -0500

    renamed folders and files

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ fetch('projects/projects.json')
             project.classList.add('sub-container')
 
             const title = data[i].title
-            const dashedTitle = title.replace(" ", "-")
+            const dashedTitle = title.replaceAll(" ", "-")
 
             project.innerHTML = 
             `
-            <img src="/projects/images/${dashedTitle}.png" alt="${data[i].title}"></img>
-            <a class="title" href="projects/${data[i].demo}">${data[i].title}</a>
+            <img src="/projects/images/${dashedTitle}.png" alt="${title}"></img>
+            <a class="title" href="projects/${dashedTitle}">${title}</a>
             <button class="btn">
-                <a class="repo" href="https://github.com/Daniel-Ho986/mini-project-showcase/tree/main/projects/${data[i].repo}">Repository</a>
+                <a class="repo" href="https://github.com/Daniel-Ho986/mini-project-showcase/tree/main/projects/${dashedTitle}">Repository</a>
             </button>
             `
             showcase.appendChild(project)
