d743fd98e5ac823a9fea449d5681363f0edf1c01
Daniel <dhonaruto@gmail.com>
2024-02-12 22:05:55 -0500

    refactor code

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ fetch('projects/projects.json')
             const project = document.createElement('div')
             project.classList.add('sub-container')
 
+            const title = data[i].title
+            const dashedTitle = title.replace(" ", "-")
+
             project.innerHTML = 
             `
-            <img src="/projects/images/${data[i].image_src}" alt="${data[i].image_alt}"></img>
+            <img src="/projects/images/${dashedTitle}.png" alt="${data[i].title}"></img>
             <a class="title" href="projects/${data[i].demo}">${data[i].title}</a>
             <button class="btn">
-                <a class="repo" href="https://github.com/Daniel-Ho986/mini-projects-showcase/tree/main/projects/${data[i].repo}">Repository</a>
+                <a class="repo" href="https://github.com/Daniel-Ho986/mini-project-showcase/tree/main/projects/${data[i].repo}">Repository</a>
             </button>
             `
             showcase.appendChild(project)
