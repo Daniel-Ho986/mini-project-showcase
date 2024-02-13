dd73977600a941953a61a5dc9733776857885b2c
Daniel <dhonaruto@gmail.com>
2024-02-13 17:52:27 -0500

    Complete text to speech and words characters counter

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const showcase = document.getElementById('showcase');
 
-
 fetch('projects/projects.json')
-    .then(results => results.json())
+    .then(response => response.json())
     .then(data => {
-        for (let i = 0; i < data.length; i++) {
-            const project = document.createElement('div')
-            project.classList.add('sub-container')
-
-            const title = data[i].title
-            const dashedTitle = title.replaceAll(" ", "-")
-
-            project.innerHTML = 
-            `
-            <img src="/projects/images/${dashedTitle}.png" alt="${title}"></img>
-            <a class="title" href="projects/${dashedTitle}">${title}</a>
-            <button class="btn">
-                <a class="repo" href="https://github.com/Daniel-Ho986/mini-project-showcase/tree/main/projects/${dashedTitle}">Repository</a>
-            </button>
-            `
-            showcase.appendChild(project)
-        }
+        data.sort();
         
-    });
+        data.forEach(title => {
+            const dashedTitle = title.replaceAll(" ", "-");
+
+            const projectElement = document.createElement('div');
+            projectElement.classList.add('sub-container');
+
+            projectElement.innerHTML = `
+                <img src="../images/${dashedTitle}.png" alt="${title}">
+                <a class="title" href="projects/${dashedTitle}">${title}</a>
+                <button class="btn">
+                    <a class="repo" href="https://github.com/Daniel-Ho986/mini-project-showcase/tree/main/projects/${dashedTitle}">Repository</a>
+                </button>
+            `;
+            
+            showcase.appendChild(projectElement);
+        });
+    })
+    .catch(error => console.error('Error fetching projects:', error));
